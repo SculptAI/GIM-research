aa2ca133b0cea20056662d1eac3e028abfb63397
Shichao Song <60967965+Ki-Seki@users.noreply.github.com>
2025-10-20 22:40:49 +0800

    feat: add new evaluation scripts for medmcqa and qasc datasets (#22)
    * feat: add new evaluation scripts for medmcqa and qasc datasets
    
    * style: re-format

--- eval/commands.sh
+++ eval/commands.sh
@@ -1,3 +1,11 @@
+# !/bin/bash
+
+# Count of examples in each dataset split:
+# #(Idavidrein/gpqa/gpqa_diamond/train) = 198
+# #(openlifescienceai/medmcqa/validation) = 4183
+# #(TIGER-Lab/MMLU-Pro/test) = 12102
+# #(allenai/qasc/validation) = 920
+
 make serve model_path=artifacts/09251-gim-sft-tmp/sft-gim
 python -m eval.mcqa.gpqa_diamond --model_name artifacts/09251-gim-sft-tmp/sft-gim --is_gim --reason_budget 2 --num_proc 40 --first_n 198
 python -m eval.mcqa.gpqa_diamond --model_name artifacts/09251-gim-sft-tmp/sft-gim --is_gim --reason_budget 4 --num_proc 40 --first_n 198
@@ -20,4 +28,6 @@ python -m eval.mcqa.mmlu_pro --model_name artifacts/09251-gim-sft-tmp/sft-gim --
 
 make serve model_path=unsloth/Qwen3-4B-Instruct-2507
 python -m eval.mcqa.gpqa_diamond --model_name unsloth/Qwen3-4B-Instruct-2507 --num_proc 40 --first_n 198
+python -m eval.mcqa.medmcqa --model_name unsloth/Qwen3-4B-Instruct-2507 --num_proc 40 --first_n 1000
 python -m eval.mcqa.mmlu_pro --model_name unsloth/Qwen3-4B-Instruct-2507 --num_proc 40 --first_n 1000
+python -m eval.mcqa.qasc --model_name unsloth/Qwen3-4B-Instruct-2507 --num_proc 40 --first_n 920
