adb7a4a30ffab17612aabbaddddee61cc0554ef2
Shichao Song <60967965+Ki-Seki@users.noreply.github.com>
2025-10-20 21:49:25 +0800

    refactor: adjust eval structure (#20)

--- eval/commands.sh
+++ eval/commands.sh
@@ -1,23 +1,23 @@
 make serve model_path=artifacts/09251-gim-sft-tmp/sft-gim
-python eval/gpqa_diamond.py --model_name artifacts/09251-gim-sft-tmp/sft-gim --is_gim --reason_budget 2 --num_proc 40 --first_n 198
-python eval/gpqa_diamond.py --model_name artifacts/09251-gim-sft-tmp/sft-gim --is_gim --reason_budget 4 --num_proc 40 --first_n 198
-python eval/gpqa_diamond.py --model_name artifacts/09251-gim-sft-tmp/sft-gim --is_gim --reason_budget 6 --num_proc 40 --first_n 198
-python eval/gpqa_diamond.py --model_name artifacts/09251-gim-sft-tmp/sft-gim --is_gim --reason_budget 8 --num_proc 40 --first_n 198
-python eval/gpqa_diamond.py --model_name artifacts/09251-gim-sft-tmp/sft-gim --is_gim --reason_budget 10 --num_proc 40 --first_n 198
-python eval/gpqa_diamond.py --model_name artifacts/09251-gim-sft-tmp/sft-gim --is_gim --reason_budget 12 --num_proc 40 --first_n 198
-python eval/gpqa_diamond.py --model_name artifacts/09251-gim-sft-tmp/sft-gim --is_gim --reason_budget 14 --num_proc 40 --first_n 198
-python eval/gpqa_diamond.py --model_name artifacts/09251-gim-sft-tmp/sft-gim --is_gim --reason_budget 16 --num_proc 40 --first_n 198
-python eval/gpqa_diamond.py --model_name artifacts/09251-gim-sft-tmp/sft-gim --is_gim --reason_budget 18 --num_proc 40 --first_n 198
-python eval/mmlu_pro.py --model_name artifacts/09251-gim-sft-tmp/sft-gim --is_gim --reason_budget 2 --num_proc 40 --first_n 1000
-python eval/mmlu_pro.py --model_name artifacts/09251-gim-sft-tmp/sft-gim --is_gim --reason_budget 4 --num_proc 40 --first_n 1000
-python eval/mmlu_pro.py --model_name artifacts/09251-gim-sft-tmp/sft-gim --is_gim --reason_budget 6 --num_proc 40 --first_n 1000
-python eval/mmlu_pro.py --model_name artifacts/09251-gim-sft-tmp/sft-gim --is_gim --reason_budget 8 --num_proc 40 --first_n 1000
-python eval/mmlu_pro.py --model_name artifacts/09251-gim-sft-tmp/sft-gim --is_gim --reason_budget 10 --num_proc 40 --first_n 1000
-python eval/mmlu_pro.py --model_name artifacts/09251-gim-sft-tmp/sft-gim --is_gim --reason_budget 12 --num_proc 40 --first_n 1000
-python eval/mmlu_pro.py --model_name artifacts/09251-gim-sft-tmp/sft-gim --is_gim --reason_budget 14 --num_proc 40 --first_n 1000
-python eval/mmlu_pro.py --model_name artifacts/09251-gim-sft-tmp/sft-gim --is_gim --reason_budget 16 --num_proc 40 --first_n 1000
-python eval/mmlu_pro.py --model_name artifacts/09251-gim-sft-tmp/sft-gim --is_gim --reason_budget 18 --num_proc 40 --first_n 1000
+python -m eval.mcqa.gpqa_diamond --model_name artifacts/09251-gim-sft-tmp/sft-gim --is_gim --reason_budget 2 --num_proc 40 --first_n 198
+python -m eval.mcqa.gpqa_diamond --model_name artifacts/09251-gim-sft-tmp/sft-gim --is_gim --reason_budget 4 --num_proc 40 --first_n 198
+python -m eval.mcqa.gpqa_diamond --model_name artifacts/09251-gim-sft-tmp/sft-gim --is_gim --reason_budget 6 --num_proc 40 --first_n 198
+python -m eval.mcqa.gpqa_diamond --model_name artifacts/09251-gim-sft-tmp/sft-gim --is_gim --reason_budget 8 --num_proc 40 --first_n 198
+python -m eval.mcqa.gpqa_diamond --model_name artifacts/09251-gim-sft-tmp/sft-gim --is_gim --reason_budget 10 --num_proc 40 --first_n 198
+python -m eval.mcqa.gpqa_diamond --model_name artifacts/09251-gim-sft-tmp/sft-gim --is_gim --reason_budget 12 --num_proc 40 --first_n 198
+python -m eval.mcqa.gpqa_diamond --model_name artifacts/09251-gim-sft-tmp/sft-gim --is_gim --reason_budget 14 --num_proc 40 --first_n 198
+python -m eval.mcqa.gpqa_diamond --model_name artifacts/09251-gim-sft-tmp/sft-gim --is_gim --reason_budget 16 --num_proc 40 --first_n 198
+python -m eval.mcqa.gpqa_diamond --model_name artifacts/09251-gim-sft-tmp/sft-gim --is_gim --reason_budget 18 --num_proc 40 --first_n 198
+python -m eval.mcqa.mmlu_pro --model_name artifacts/09251-gim-sft-tmp/sft-gim --is_gim --reason_budget 2 --num_proc 40 --first_n 1000
+python -m eval.mcqa.mmlu_pro --model_name artifacts/09251-gim-sft-tmp/sft-gim --is_gim --reason_budget 4 --num_proc 40 --first_n 1000
+python -m eval.mcqa.mmlu_pro --model_name artifacts/09251-gim-sft-tmp/sft-gim --is_gim --reason_budget 6 --num_proc 40 --first_n 1000
+python -m eval.mcqa.mmlu_pro --model_name artifacts/09251-gim-sft-tmp/sft-gim --is_gim --reason_budget 8 --num_proc 40 --first_n 1000
+python -m eval.mcqa.mmlu_pro --model_name artifacts/09251-gim-sft-tmp/sft-gim --is_gim --reason_budget 10 --num_proc 40 --first_n 1000
+python -m eval.mcqa.mmlu_pro --model_name artifacts/09251-gim-sft-tmp/sft-gim --is_gim --reason_budget 12 --num_proc 40 --first_n 1000
+python -m eval.mcqa.mmlu_pro --model_name artifacts/09251-gim-sft-tmp/sft-gim --is_gim --reason_budget 14 --num_proc 40 --first_n 1000
+python -m eval.mcqa.mmlu_pro --model_name artifacts/09251-gim-sft-tmp/sft-gim --is_gim --reason_budget 16 --num_proc 40 --first_n 1000
+python -m eval.mcqa.mmlu_pro --model_name artifacts/09251-gim-sft-tmp/sft-gim --is_gim --reason_budget 18 --num_proc 40 --first_n 1000
 
 make serve model_path=unsloth/Qwen3-4B-Instruct-2507
-python eval/gpqa_diamond.py --model_name unsloth/Qwen3-4B-Instruct-2507 --num_proc 40 --first_n 198
-python eval/mmlu_pro.py --model_name unsloth/Qwen3-4B-Instruct-2507 --num_proc 40 --first_n 1000
+python -m eval.mcqa.gpqa_diamond --model_name unsloth/Qwen3-4B-Instruct-2507 --num_proc 40 --first_n 198
+python -m eval.mcqa.mmlu_pro --model_name unsloth/Qwen3-4B-Instruct-2507 --num_proc 40 --first_n 1000
