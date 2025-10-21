8cee09c5b0132882e0963f1bf78d354bfacea647
Shichao Song <60967965+Ki-Seki@users.noreply.github.com>
2025-10-21 21:03:46 +0800

    feat: add Composite Text Perplexity (CTP) evaluation functionality and dataset loading script (#24)
    * feat: add Composite Text Perplexity (CTP) evaluation functionality and dataset loading script
    
    * refactor: rename reference model argument and update evaluator logic
    
    * fix: add missing dataset example count for Sculpt-AI in commands.sh
    
    * refactor: update return type of _model_call method to str

--- eval/commands.sh
+++ eval/commands.sh
@@ -1,12 +1,14 @@
 # !/bin/bash
 
 # Count of examples in each dataset split:
+# #(Sculpt-AI/GIM-SFT/*/train) = 3157829
 # #(Idavidrein/gpqa/gpqa_diamond/train) = 198
 # #(openlifescienceai/medmcqa/validation) = 4183
 # #(TIGER-Lab/MMLU-Pro/test) = 12102
 # #(allenai/qasc/validation) = 920
 
 make serve model_path=artifacts/09251-gim-sft-tmp/sft-gim
+python -m eval.ctp.gim_sft --model_name artifacts/09251-gim-sft-tmp/sft-gim --is_gim --ref_model_device cpu --first_n 100
 python -m eval.mcqa.gpqa_diamond --model_name artifacts/09251-gim-sft-tmp/sft-gim --is_gim --reason_budget 2 --num_proc 40 --first_n 198
 python -m eval.mcqa.gpqa_diamond --model_name artifacts/09251-gim-sft-tmp/sft-gim --is_gim --reason_budget 4 --num_proc 40 --first_n 198
 python -m eval.mcqa.gpqa_diamond --model_name artifacts/09251-gim-sft-tmp/sft-gim --is_gim --reason_budget 6 --num_proc 40 --first_n 198
