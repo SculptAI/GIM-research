b53e5a45091bf67cd97a01629d96e120abe69c90
Shichao Song <60967965+Ki-Seki@users.noreply.github.com>
2025-10-23 16:53:59 +0800

    fix: streamline command execution in mixture run scripts

--- runs/2510231-mixture-high/commands.sh
+++ runs/2510231-mixture-high/commands.sh
@@ -1,4 +1,2 @@
 # UNSLOTH_COMPILE_DISABLE=1 debugpy runs/2510231-mixture-high/train.py
-python runs/2510231-mixture-high/train.py
-sleep 180
-/usr/bin/shutdown
+python runs/2510231-mixture-high/train.py; sudo shutdown -h +3
--- runs/2510232-mixture-mid/commands.sh
+++ runs/2510232-mixture-mid/commands.sh
@@ -1,4 +1,2 @@
 # UNSLOTH_COMPILE_DISABLE=1 debugpy runs/2510232-mixture-mid/train.py
-python runs/2510232-mixture-mid/train.py
-sleep 180
-/usr/bin/shutdown
+python runs/2510232-mixture-mid/train.py; sudo shutdown -h +3
